7a75676c58288ebce2db05da872f7f3b6dcbed2e
YUapisit <aps.apisit250@gmail.com>
2023-03-20 20:18:55 +0700

    'reproject'

--- cafe/cafe/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import *
-
-# Register your models here.
-
-admin.site.register(Menu)
-admin.site.register(Categories)
-admin.site.register(Orders)
-admin.site.register(Carts)
